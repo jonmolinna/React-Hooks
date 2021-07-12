f409ad1b118884ca8ee30cf93f29ad487931337b
jonmolinna <jmolina2624@gmail.com>
2021-07-12 17:27:30 -0500

    src

--- 1.-react-basico/src/App.js
+++ 1.-react-basico/src/App.js
@@ -4,6 +4,12 @@ import ContadorHooks from './components/ContadorHook';
 import ScrollHooks from './components/ScrollHooks';
 import RelojHooks from './components/RelojHooks';
 import AjaxHooks from './components/AjaxHook';
+import HooksPersonalizados from './components/HooksPersonalizados';
+import Referencias from './components/Referencias';
+import Formularios from './components/Formularios';
+import Formularios2 from './components/Formularios2';
+import Estilos from './components/Estilos';
+import ComponentesEstilizados from './components/ComponentesEstilizados';
 
 function App() {
   return (
@@ -14,6 +20,28 @@ function App() {
       <RelojHooks />
       <hr />
       <AjaxHooks />
+      <hr />
+      <HooksPersonalizados />
+      <hr />
+      <Referencias />
+      <hr />
+      <Formularios />
+      <hr />
+      <Formularios2 />
+      <hr />
+      <Estilos />
+      <hr />
+      <ComponentesEstilizados />
+      <hr />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
