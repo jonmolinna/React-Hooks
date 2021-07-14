5ee29d28f66ecb1d5f79108276bf237d8d258061
jonmolinna <jmolina2624@gmail.com>
2021-07-13 19:53:38 -0500

    src

---
+++ 2.-react-frameworks-css/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+//import Bootstrap from './components/Bootstrap';
+import Bulma from './components/Bulma';
+
+function App() {
+  return (
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap /> */}
+      <Bulma />
+    </div>
+  );
+}
+
+export default App;
+
+
+// Ya sabes material ui
+// Ya sabes usar usar bootstrap con npm
