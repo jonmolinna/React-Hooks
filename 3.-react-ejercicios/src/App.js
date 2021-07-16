c3aa84f07cd820e7632e8024d931837c6b54ea07
jonmolinna <jmolina2624@gmail.com>
2021-07-16 17:12:27 -0500

    src

---
+++ 3.-react-ejercicios/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CrudApp from './components/CrudApp';
+
+function App() {
+  return (
+    <>
+      <h1>Ejercicios con React</h1>
+      <CrudApp />
+      <hr />
+    </>
+  );
+}
+
+export default App;
