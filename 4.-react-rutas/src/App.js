b278bf848122ca3b2276f687398a8040b936d977
jonmolinna <jmolina2624@gmail.com>
2021-07-23 17:33:03 -0500

    React Rutas

---
+++ 4.-react-rutas/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ConceptosBasicos from './components/ConceptosBasicos';
+
+function App() {
+  return (
+    <div>
+      <h1>React Router</h1>
+      <a href="https://reactrouter.com/web/guides/quick-start" target="_blank" rel="noreferrer">Documentacion</a>
+      <hr />
+      <ConceptosBasicos />
+    </div>
+  );
+}
+
+export default App;
