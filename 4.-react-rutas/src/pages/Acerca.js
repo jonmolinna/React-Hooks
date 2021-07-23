e2c1014a2ea7c5b0785e7295c5396b391cd8f95c
jonmolinna <jmolina2624@gmail.com>
2021-07-23 17:32:16 -0500

    Pages

---
+++ 4.-react-rutas/src/pages/Acerca.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Acerca = () => {
+    return (
+        <div>
+            <h2>Acerca</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At velit voluptate,
+                soluta fugit recusandae expedita sint ratione ut, est saepe reprehenderit
+                inventore ea sed sequi eius incidunt deleniti consequuntur magni.
+            </p>
+        </div>
+    )
+}
+
+export default Acerca;
