d0ae7b0fce8602f90945ed01acb0563d3ed025e0
jonmolinna <jmolina2624@gmail.com>
2021-08-02 18:12:19 -0500

    Con Context

---
+++ 7.-react-context/src/components/MainContext.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
+import LanguageContext from '../context/LanguageContext';
+import AuthContext from '../context/AuthContext';
+
+const MainContext = () => {
+    const { theme } = useContext(ThemeContext);
+    const { texts } = useContext(LanguageContext);
+    const { auth } = useContext(AuthContext);
+
+    return (
+        <main className={theme}>
+            {
+                auth?
+                <p>{texts.mainWelcome}</p> :
+                <p>{texts.mainHello}</p>
+            }
+            <p>{texts.mainContent}</p>
+        </main>
+    )
+}
+
+export default MainContext;
