a487fc48f2f2b44cd6e26f35acdd5b58e92af8f3
jonmolinna <jmolina2624@gmail.com>
2021-08-02 15:59:48 -0500

    Sin Context

---
+++ 7.-react-context/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Main = ({ theme, texts, auth }) => {
+    return (
+        <main className={theme}>
+            {
+                auth?
+                <p>{texts.mainWelcome}</p> :
+                <p>{texts.mainHello}</p>
+            }
+            <p>{texts.mainContent}</p>
+        </main>
+    )
+}
+
+export default Main;
---
+++ 7.-react-context/src/components/MyPage.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+
+const initialTheme = "light";
+const initialLanguage = "es";
+const initialAuth = null;
+
+const translations = {
+    es: {
+        headerTitle: "Mi aplicacion SIN Context API",
+        headerSubtitle: "Mi cabecera",
+        headerLight: "Claro",
+        headerDark: "Oscuro",
+        buttonLogin: "Iniciar Sesión",
+        buttonLogout: "Cerrar Sesion",
+        mainWelcome: "Bievenido invitado",
+        mainHello: "Hola Usuario",
+        mainContent: "Mi Contenido principal",
+        footerTitle: "Mi pie de pagina",
+    },
+    en: {
+        headerTitle: "My application without Context API",
+        headerSubtitle: "My header",
+        headerLight: "Light",
+        headerDark: "Dark",
+        buttonLogout: "Logout",
+        mainWelcome: "Welcome Guest",
+        mainHello: "Hello User",
+        mainContent: "My main content",
+        footerTitle: "My footer"
+    },
+};
+
+const MyPage = () => {
+    const [theme, setTheme] = useState(initialTheme);
+    const [language, setLanguage] = useState(initialLanguage);
+    const [texts, setTexts] = useState(translations[language]);
+    const [auth, setAuth] = useState(initialAuth);
+    
+    const handleTheme = (e) => {
+        //console.log(e.target.value);
+        if(e.target.value === "light"){
+            setTheme("light")
+        } else {
+            setTheme("dark")
+        }
+    };
+
+    const handleLanguage = (e) => {
+        //console.log(e.target.value);
+        if(e.target.value === "es"){
+            setLanguage("es");
+            setTexts(translations.es)
+        } else {
+            setLanguage("en");
+            setTexts(translations.en)
+        }
+    };
+
+    const handleAuth = (e) => {
+        if(auth){
+            setAuth(null)
+        } else {
+            setAuth(true);
+        }
+    };
+
+    return (
+        <div className="my-page">
+            <Header 
+                theme={theme} 
+                handleTheme={handleTheme} 
+                texts={texts} 
+                handleLanguage={handleLanguage} 
+                auth={auth}
+                handleAuth={handleAuth}
+            />
+            <Main 
+                theme={theme} 
+                texts={texts}
+                auth={auth} 
+            />
+            <Footer theme={theme} texts={texts} />
+        </div>
+    )
+}
+
+export default MyPage;
