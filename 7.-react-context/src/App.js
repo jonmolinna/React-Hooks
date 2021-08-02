a0017a1c046104d5e4afda13e0e6d11e32ad05d1
jonmolinna <jmolina2624@gmail.com>
2021-08-02 18:13:27 -0500

    React Context

---
+++ 7.-react-context/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MyPage from './components/MyPage';
+import MyPageContext from './components/MyPageContext';
+
+function App() {
+  return (
+    <div>
+      <h1>React Context API</h1>
+      <a href="https://es.reactjs.org/docs/context.html" target="_blank" rel="noreferrer">Documentacion</a>
+      <hr />
+      <MyPageContext />
+      <hr />
+      <MyPage />
+    </div>
+  );
+}
+
+export default App;
