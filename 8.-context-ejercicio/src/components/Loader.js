da610a2dcf8b7fdb5b71ad9413fc1264143c6cc7
jonmolinna <jmolina2624@gmail.com>
2021-08-03 15:57:50 -0500

    components

---
+++ 8.-context-ejercicio/src/components/Loader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loadingio-spinner-spinner-pawyphwdia">
+            <div className="ldio-x3w6fiknjh">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
