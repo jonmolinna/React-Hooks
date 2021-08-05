b8420b41b389a77a3d329d5541cd2bae2cbf17c6
jonmolinna <jmolina2624@gmail.com>
2021-08-05 17:43:13 -0500

    React Reducers

--- 9.-react-reducers/src/App.js
+++ 9.-react-reducers/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Contador from './components/Contador';
 import ContadorMejorada from './components/ContadorMejorada';
+import CrudApi from './components/CrudApi';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <div>
       <h1>useReducer</h1>
+      <hr />
+      <CrudApi />
       <hr />
       <ShoppingCart />
       <hr />
