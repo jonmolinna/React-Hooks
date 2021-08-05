585dbf9c6a66e2985e29866a876170b328cd18fa
jonmolinna <jmolina2624@gmail.com>
2021-08-05 15:38:15 -0500

    React Reducers

--- 9.-react-reducers/src/App.js
+++ 9.-react-reducers/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Contador from './components/Contador';
 import ContadorMejorada from './components/ContadorMejorada';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <div>
       <h1>useReducer</h1>
+      <hr />
+      <ShoppingCart />
       <hr />
       <ContadorMejorada />
       <hr />
