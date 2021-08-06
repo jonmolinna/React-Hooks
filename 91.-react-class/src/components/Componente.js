4b7eee1d07454ad89ee82a9af920396c42c254bc
jonmolinna <jmolina2624@gmail.com>
2021-08-06 15:49:13 -0500

    Componente

---
+++ 91.-react-class/src/components/Componente.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Componente extends Component {
+    render(){
+        return(
+            <h2>{this.props.msg}</h2>
+        )
+    }
+};
+
+export default Componente;
